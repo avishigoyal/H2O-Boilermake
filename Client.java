0f69028f99f7a131645e2357d9bb16f3b991ffc8
Bhavya Venkat <bhavya.v04@gmail.com>
2023-01-20 20:56:09 -0800

    commit

--- Client.java
+++ Client.java
@@ -1,8 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter a host name: ");
         String host = scan.nextLine();
         System.out.println("Enter a port number: ");
@@ -10,6 +16,15 @@ public class Client {
         scan.nextLine();
         try {
             Socket socket = new Socket(host, portNumber);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            System.out.println("Log in or Sign up?");
+            System.out.println("1. Log in\n2. Sign up");
+            int choice = scan.nextInt();
+            scan.nextLine();
+            switch(choice) {
+                case 1:
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }

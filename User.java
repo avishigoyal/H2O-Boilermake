1e23554f0d2bc0d4f19d1e4e4fbf83b1d8154f91
avishigoyal <goyal103@purdue.edu>
2023-01-21 00:03:02 -0500

    fileUtils

--- User.java
+++ User.java
@@ -15,5 +15,44 @@ public class User implements Serializable {
         this.county = county;
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
 
 }
